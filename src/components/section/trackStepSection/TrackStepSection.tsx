927c5ab5dc16e187b167696d80730cb7b663cc07
hadarl <hadarl@rapyd.net>
2022-05-01 22:44:36 +0300

    add related emails and notes

--- src/components/section/trackStepSection/TrackStepSection.tsx
+++ src/components/section/trackStepSection/TrackStepSection.tsx
@@ -1,30 +1,108 @@
-import React from "react";
+import React, { useState } from "react";
 import TrackStepSectionStyled from "./TrackStepSectionStyled";
 import TitleSection from "../titleSection/TitleSection";
 import SectionContent from "../sectionContent/SectionContent";
 import Button from "../../common/button/Button"
+import { useEffect } from "@storybook/addons";
 
 interface TrackStepSectionProps {
   title: string,
+  step: string,
   description: string,
   date: string,
-  comments?: string[],
-  relatedEmails?:string[]
-
+  relatedEmails?: string[],
+  notes?: string[]
 }
 const TrackStepSection: React.FC<TrackStepSectionProps> = (props) => {
+
+  const [notes, setNotes]=useState([props.notes]);
+  const [newNote, setNewNote]=useState('');
+  const [addNoteInput, setAddNoteInput] = React.useState(false);
+
+  const [relatedEmails, setRelatedEmails]=useState([props.relatedEmails]);
+  const [newEmail, setNewEmail]=useState('');
+  const [addEmailInput, setAddEmailInput]=useState(false);
+
+
+  function handleAddNoteChange(event:any){
+    setNewNote(event.target.value);
+  }
+
+  function handleAddNoteClick(){
+    if(newNote!==""){
+      setNotes([...notes,newNote] as any);
+    }
+    setNewNote("");
+  }
+
+  function handleAddEmailChange(event:any){
+    setNewEmail(event.target.value);
+  }
+
+  function handleAddEmailClick(){
+    if(newEmail!==""){
+      setRelatedEmails([...relatedEmails,newEmail] as any);
+    }
+    setNewEmail("");
+  }
+
+  function showNoteInput(){
+    setAddNoteInput(!addNoteInput);
+  }
+
+  function showEmailInput(){
+    setAddEmailInput(!addEmailInput);
+
+  }
+
+  
+
   return (
       <TrackStepSectionStyled>
        <div className="p-5 mb-4 bg-light rounded-3">
         <div className="container-fluid py-5">
           <TitleSection title={`${props.title} ${props.date}`}/>
           <SectionContent content={props.description} />
-          <div className="btn-new-item">
-            <Button
-              style="primary"
+          <h3>related emails:</h3>
+          <div>{relatedEmails.map((email) => <li>{email}</li> )}</div>
+          {
+            addEmailInput?
+            <div>
+          <input type="text" value={newEmail} onChange={handleAddEmailChange} />
+          <Button
+              style="success"
               size="lg"
-              title="Add comment"
-              onClick={()=>{console.log("clicked")}}
+              title="+"
+              onClick={handleAddEmailClick}
+            />
+              </div>
+              :null
+          }
+        <Button
+              style={addEmailInput? "danger" : "primary"}
+              size="lg"
+              title={addEmailInput ? "X" : "Add email"}
+              onClick={showEmailInput}
+            />
+          <h3>notes:</h3>
+          <div>{notes.map((note) => <li>{note}</li> )}</div>
+          <div className="btn-new-item">
+          {addNoteInput?
+          <div >
+             <input type="text" value={newNote} onChange={handleAddNoteChange} /> 
+             <Button
+              style="success"
+              size="lg"
+              title="+"
+              onClick={handleAddNoteClick}
+            />
+          </div>
+                :null}
+            <Button
+              style={addNoteInput ? "danger" : "primary"}
+              size="lg"
+              title={addNoteInput ? "X" : "Add note"}
+              onClick={showNoteInput}
             />
           </div>
         </div>
--- src/pages/TrackStepPage.tsx
+++ src/pages/TrackStepPage.tsx
@@ -5,13 +5,20 @@ import TrackStepSection from "../components/section/trackStepSection/TrackStepSe
 const RecruitmentTracksStepPage = () => {
   return (
     <React.Fragment>
-      <TrackStepSection title="Microsoft - Full Stack Developer" 
+      <TrackStepSection 
+      title="Microsoft - Full Stack Developer" 
+      step="HR intreview"
       description="Lorem Ipsum is simply dummy text
        of the printing and typesetting industry.
         Lorem Ipsum has been the industry's standard 
+        took a galley of type and scrambled it to make a type specimen book. typesetting industry.
+        Lorem Ipsum has been the industry's standard 
         took a galley of type and scrambled it to make a type specimen book."
-        date="12/2/2022" />
-    
+        relatedEmails={["hadar@gmail.com"]}
+        notes={["call to the hr","read about typescript"]}
+        date="12/2/2022"
+        />
+
     </React.Fragment>
   );
 };
