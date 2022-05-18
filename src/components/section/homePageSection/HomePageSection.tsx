521afb4469594b793f4fe221aefe3d69f97ec0dd
yohai109 <yohai109@gmail.com>
2022-05-18 13:07:18 +0300

    the search in the main screen now searches by the company name, position name and position description (#20)

--- src/components/section/homePageSection/HomePageSection.tsx
+++ src/components/section/homePageSection/HomePageSection.tsx
@@ -6,8 +6,8 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import Checkbox from "@mui/material/Checkbox";
-import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
-import Favorite from '@mui/icons-material/Favorite';
+import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
+import Favorite from "@mui/icons-material/Favorite";
 import IconButton from "@mui/material/IconButton";
 import CommentIcon from "@mui/icons-material/Comment";
 import Grid from "@mui/material/Grid";
@@ -16,12 +16,12 @@ import { useSelector } from "react-redux";
 import { State } from "../../../state";
 import Track from "../../../models/Track";
 import { useNavigate } from "react-router-dom";
-import {HomePageSectionStyled, positionTitle} from "./HomePageSectionStyled";
-import Button from '@mui/material/Button';
-import AddBoxIcon from '@mui/icons-material/AddBox';
-import SearchIcon from '@mui/icons-material/Search';
-import InputAdornment from '@mui/material/InputAdornment';
-import KeyboardIcon from '@mui/icons-material/Keyboard';
+import { HomePageSectionStyled, positionTitle } from "./HomePageSectionStyled";
+import Button from "@mui/material/Button";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+import SearchIcon from "@mui/icons-material/Search";
+import InputAdornment from "@mui/material/InputAdornment";
+import KeyboardIcon from "@mui/icons-material/Keyboard";
 
 const HomePageSection: React.FC = () => {
   let navigation = useNavigate();
@@ -44,100 +44,98 @@ const HomePageSection: React.FC = () => {
     navigation("/recruitment-track-page", { state: track });
   };
 
-  const handleAddTrack = () =>{
+  const handleAddTrack = () => {
     navigation("/create-recruitment-track-page");
   };
 
+  const searchFunction = (track: Track, queary: string) => {
+    const searchTerm = queary.toLowerCase()
+    return (
+      track.companyName.toLowerCase().includes(searchTerm) ||
+      track.position.name.toLowerCase().includes(searchTerm) ||
+      track.position.description.toLowerCase().includes(searchTerm)
+    );
+  };
 
   return (
-  <HomePageSectionStyled>
-    <div>
-      <Grid container spacing={3} justifyContent="center" alignItems="center">
-      <Grid item container >
-        <h1 className="welcomeTitle"> Welcome back Username,</h1>
-      </Grid>
-
-        <Grid item container>
-        <h3 className="activePositionsTitle"> Active positions:</h3>
-
-        </Grid>
-        <Grid container item xs={12}>
-      
-          <TextField
-            id="outlined-basic"
-            label="search position"
-            className="searchPosition"
-            InputProps={{
-              startAdornment: (
-                <InputAdornment
-                 position="start"
-                 >
-                  <SearchIcon />
-                </InputAdornment>
-              ),
-            }}
-            onChange={handleSetSearch}
-          />
-          
-        </Grid>
-        
-        <Grid item width={"50%"}>
-          <List>
-            {(tracks as Array<Track>).map((currTrack: Track) => {
-              return (
-                <div>
-                  {currTrack.id.includes(search) ? (
-                    <>
-                    <ListItem
-                      
-                      secondaryAction={
-                        <Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite className="favoriteIcon"/>} />
-                     
-                      } 
-                    >
-                      <ListItemButton>
-                      <KeyboardIcon/>
-                        <ListItemText 
-                         primary={`${currTrack.companyName} - ${currTrack.position.name}`}
-                        
-                        primaryTypographyProps={positionTitle}
-                        onClick={() => {
-                        handleClick(currTrack);
-                      }}/>
-                      </ListItemButton>
-
-                    </ListItem>
-                    
-                      </>
-                  ) : (
-                    <div></div>
-                  )}
-                </div>
-              );
-            })}
-          </List>
-        </Grid>
-        
-      </Grid>
-      <Grid container item >
-  
-        <Button 
-        className="addNewTrackButton"
-        variant="contained"
-        startIcon={<AddBoxIcon />}
-        onClick={() => {
-          handleAddTrack();
-        }}
-        >
-          new recruitment track
-          </Button>
-
-     
+    <HomePageSectionStyled>
+      <div>
+        <Grid container spacing={3} justifyContent="center" alignItems="center">
+          <Grid item container>
+            <h1 className="welcomeTitle"> Welcome back Username,</h1>
           </Grid>
 
-    </div>
-    </HomePageSectionStyled>
+          <Grid item container>
+            <h3 className="activePositionsTitle"> Active positions:</h3>
+          </Grid>
+          <Grid container item xs={12}>
+            <TextField
+              id="outlined-basic"
+              label="search position"
+              className="searchPosition"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }}
+              onChange={handleSetSearch}
+            />
+          </Grid>
 
+          <Grid item width={"50%"}>
+            <List>
+              {(tracks as Array<Track>).map((currTrack: Track) => {
+                return (
+                  <div>
+                    {searchFunction(currTrack, search) ? (
+                      <>
+                        <ListItem
+                          secondaryAction={
+                            <Checkbox
+                              icon={<FavoriteBorder />}
+                              checkedIcon={
+                                <Favorite className="favoriteIcon" />
+                              }
+                            />
+                          }
+                        >
+                          <ListItemButton>
+                            <KeyboardIcon />
+                            <ListItemText
+                              primary={`${currTrack.companyName} - ${currTrack.position.name}`}
+                              primaryTypographyProps={positionTitle}
+                              onClick={() => {
+                                handleClick(currTrack);
+                              }}
+                            />
+                          </ListItemButton>
+                        </ListItem>
+                      </>
+                    ) : (
+                      <div></div>
+                    )}
+                  </div>
+                );
+              })}
+            </List>
+          </Grid>
+        </Grid>
+        <Grid container item>
+          <Button
+            className="addNewTrackButton"
+            variant="contained"
+            startIcon={<AddBoxIcon />}
+            onClick={() => {
+              handleAddTrack();
+            }}
+          >
+            new recruitment track
+          </Button>
+        </Grid>
+      </div>
+    </HomePageSectionStyled>
   );
 };
 
