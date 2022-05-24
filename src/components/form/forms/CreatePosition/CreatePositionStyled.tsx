cab35a5092c4a3062e1607e491c267bec047bd74
hadarl <hadarl@rapyd.net>
2022-05-24 20:45:28 +0300

    add tags to create position page

--- src/components/form/forms/CreatePosition/CreatePositionStyled.tsx
+++ src/components/form/forms/CreatePosition/CreatePositionStyled.tsx
@@ -1,7 +1,19 @@
 import styled from "styled-components";
 
 const CreatePosition = styled.div`
-  
+
+  & .container{
+    font-family: Gill Sans;
+    margin: auto;
+    width:50%;
+
+    }
+
+  & .requiresTags{
+      margin-top:20px;
+      margin:auto;
+      font-size:2em;
+  }
 `;
 
 export default CreatePosition;
