ca0fd00c95dc37707118a79615a7290b91e0f1b6
Yohai Knaani <yohai109@gmail.com>
2022-06-05 16:45:45 +0300

    make the suggestions list a table

--- src/components/section/position/positionSection.tsx
+++ src/components/section/position/positionSection.tsx
@@ -1,19 +1,24 @@
 import AssignmentIcon from "@mui/icons-material/Assignment";
-import SendIcon from "@mui/icons-material/Send";
 import { TimelineItem } from "@mui/lab";
 import Timeline from "@mui/lab/Timeline";
 import TimelineConnector from "@mui/lab/TimelineConnector";
 import TimelineContent from "@mui/lab/TimelineContent";
 import TimelineDot from "@mui/lab/TimelineDot";
 import TimelineSeparator from "@mui/lab/TimelineSeparator";
-import { Alert, Fab, Snackbar, TextField, Typography } from "@mui/material";
-import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import OutlinedInput from "@mui/material/OutlinedInput";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
-import { Theme, useTheme } from "@mui/material/styles";
-import React, { useEffect } from "react";
+import {
+    Alert,
+    Checkbox,
+    Paper,
+    Snackbar,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    Typography,
+} from "@mui/material";
+import React, { ChangeEvent, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
 import Track from "../../../models/Track";
@@ -35,23 +40,22 @@ const PositionSection: React.FC = () => {
     const location = useLocation();
     const currUser = getCurrentUser();
     const positionId: string = location.state as string;
-    const [isShareShown, setShowResults] = React.useState(false);
     const [open, setOpen] = React.useState(false);
 
     const position = useSelector((state: State) => state.companys)
         .find((curr) => curr._id === currUser.company)
         ?.positions?.find((curr) => curr._id === positionId)!!;
 
-    const createTrack = async  (users: string[]) => {
-        console.log({users});
+    const createTrack = async (users: string[]) => {
+        console.log({ users });
         const usersToMatch = suggestions.filter((curr) => {
             return users.includes(curr.user.userName);
         });
-        console.log({usersToMatch});
+        console.log({ usersToMatch });
 
         const newTrack: Track = {
             company: "",
-            position: {...position},
+            position: { ...position },
             isActive: true,
             isFavorite: false,
             steps: [...(position.template ? position.template : [])],
@@ -61,22 +65,13 @@ const PositionSection: React.FC = () => {
         };
 
         const userIds = usersToMatch.map((user) => user.user._id);
-        await suggestTrack(newTrack,currUser.company, userIds)
+        await suggestTrack(newTrack, currUser.company, userIds);
         setOpen(true);
     };
 
-    const [suggestions, setSuggestions] = React.useState<Array<userSuggestions>>([])
-
-    const ITEM_HEIGHT = 48;
-    const ITEM_PADDING_TOP = 8;
-    const MenuProps = {
-        PaperProps: {
-            style: {
-                maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-                width: 250,
-            },
-        },
-    };
+    const [suggestions, setSuggestions] = React.useState<
+        Array<userSuggestions>
+    >([]);
 
     async function getCompanys() {
         const { data } = await getSuggestios(
@@ -90,20 +85,6 @@ const PositionSection: React.FC = () => {
         getCompanys();
     }, []);
 
-    const names = [
-        "Oliver Hansen",
-        "Van Henry",
-        "April Tucker",
-        "Ralph Hubbard",
-        "Omar Alexander",
-        "Carlos Abbott",
-        "Miriam Wagner",
-        "Bradley Wilkerson",
-        "Virginia Andrews",
-        "Kelly Snyder",
-    ];
-
-    const theme = useTheme();
     const [personName, setPersonName] = React.useState<string[]>([]);
 
     const handleClose = (
@@ -117,27 +98,27 @@ const PositionSection: React.FC = () => {
         setOpen(false);
     };
 
-    const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+    const handleChange = (
+        event: ChangeEvent<HTMLInputElement>,
+        checked: boolean
+    ) => {
         const {
             target: { value },
         } = event;
-        setPersonName(
-            // On autofill we get a stringified value.
-            typeof value === "string" ? value.split(",") : value
-        );
-    };
+        if (checked) {
+            const valuesToAdd: string[] = [
+                ...(typeof value === "string" ? value.split(",") : value),
+            ];
 
-    function getStyles(
-        name: string | undefined,
-        personName: string[],
-        theme: Theme
-    ) {
-        return {
-            fontWeight: !personName.find((curr) => curr === name)
-                ? theme.typography.fontWeightRegular
-                : theme.typography.fontWeightMedium,
-        };
-    }
+            const newArray = [
+                ...personName,
+                ...valuesToAdd.filter((curr) => !personName.includes(curr)),
+            ];
+            setPersonName([...newArray]);
+        } else {
+            setPersonName([...personName.filter((curr) => curr !== value)]);
+        }
+    };
 
     return (
         <PositionStyled>
@@ -173,19 +154,8 @@ const PositionSection: React.FC = () => {
                     navigation("/add-step-template", { state: position });
                 }}
             />
-            {/* <div>
-                {isShareShown && (
-                    <div>
-                        <TextField label="email" />
-                        <Fab
-                            className="shareBtn"
-                            color="primary"
-                            onClick={() => createTrack()}
-                        >
-                            <SendIcon />
-                        </Fab>
-                    </div>
-                )}
+
+            <div>
                 <Button
                     title="Share"
                     color=""
@@ -193,44 +163,61 @@ const PositionSection: React.FC = () => {
                     width="170px"
                     top="32px"
                     left="100px"
-                    onClick={() => {
-                        setShowResults(!isShareShown);
-                    }}
-                />
-            </div> */}
-            <div>
-                <FormControl sx={{ m: 1, width: 300 }}>
-                    <InputLabel id="demo-multiple-name-label">Match</InputLabel>
-                    <Select
-                        labelId="demo-multiple-name-label"
-                        id="demo-multiple-name"
-                        multiple
-                        value={personName}
-                        onChange={handleChange}
-                        input={<OutlinedInput label="Match" />}
-                        MenuProps={MenuProps}
-                    >
-                        {suggestions.map((user) => (
-                            <MenuItem
-                                key={user.user._id}
-                                value={user.user.userName}
-                                style={getStyles(
-                                    user.user._id,
-                                    personName,
-                                    theme
-                                )}
-                            >
-                                {user.user.firstName} {user.user.lastName} -{" "}
-                                {user.score}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                </FormControl>
-                <Fab
-                    className="shareBtn"
-                    color="primary"
                     onClick={() => createTrack(personName)}
-                ></Fab>
+                />
+                <TableContainer
+                    sx={{ mx: "auto", mt: 1, width: 1 / 2 }}
+                    component={Paper}
+                >
+                    <Table size="small" aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell size="small" align="center">
+                                    <Checkbox />
+                                </TableCell>
+                                <TableCell align="left">Full Name</TableCell>
+                                <TableCell align="left">Email</TableCell>
+                                <TableCell align="left">CV</TableCell>
+                                <TableCell align="left">
+                                    Match Percentage
+                                </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {suggestions.map((currSuggestion) => (
+                                <TableRow
+                                    key={currSuggestion.user.email}
+                                    sx={{
+                                        "&:last-child td, &:last-child th": {
+                                            border: 0,
+                                        },
+                                    }}
+                                >
+                                    <TableCell size="small" align="center">
+                                        <Checkbox
+                                            aria-label="Checkbox demo"
+                                            value={currSuggestion.user.userName}
+                                            onChange={handleChange}
+                                        />
+                                    </TableCell>
+                                    <TableCell align="left">
+                                        {currSuggestion.user.firstName}{" "}
+                                        {currSuggestion.user.lastName}
+                                    </TableCell>
+                                    <TableCell align="left">
+                                        {currSuggestion.user.email}
+                                    </TableCell>
+                                    <TableCell align="left">
+                                        {currSuggestion.user.cvs}
+                                    </TableCell>
+                                    <TableCell align="left">
+                                        {currSuggestion.score}
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
             </div>
             <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
                 <Alert
