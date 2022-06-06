73c9a5fb89dabab566fcf1dbd4f5f317e35251b9
Chen Sabban <chensabban1@gmail.com>
2022-06-06 23:26:26 +0300

    fix-style

--- src/components/section/profileSection/ProfileSection.tsx
+++ src/components/section/profileSection/ProfileSection.tsx
@@ -4,16 +4,19 @@ import { useSelector } from "react-redux";
 import { State } from "../../../state";
 import { Avatar, Card, Grid, Typography } from "@mui/material";
 import CVsSection from "../cvsSection/CVs";
-import avatar from "../../../images/avatar.png"
+import avatar from "../../../images/avatar.png";
+import { useNavigate } from "react-router-dom";
+
 
 const ProfileSection: React.FC = () => {
 
   const currUser = useSelector((state: State) => state.loginUser);
+  const navigate = useNavigate();
 
 
   return (
     <ProfileSectionStyled>
-      <Grid container className="container" direction="column" margin="0px">
+      <Grid container className="container" direction="column" margin="0px" onClick={()=> navigate("/profile")}>
         <Card className="card">
         <Avatar alt="profileAvatar" className="avatar" src={avatar}   sx={{ width: 56, height: 56 }}/>
         <Typography className="title" variant="h6">
@@ -23,12 +26,14 @@ const ProfileSection: React.FC = () => {
         {currUser.email}
         </Typography>
       </Card>
+      {(currUser.role === "HR" && currUser)? (<></>) : (
       <Card className="card">
           <Typography className="myCvsTitle" variant="body1">
           My CV's :
         </Typography>
         <CVsSection />     
       </Card>
+      ) }
       </Grid>   
     </ProfileSectionStyled>
   );
--- src/pages/PositionsListPage.tsx
+++ src/pages/PositionsListPage.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import PositionsListSection from "../components/section/positionList/PositionListSection";
+import Grid from "@mui/material/Grid";
+import ProfileSection from "../components/section/profileSection/ProfileSection";
 
 const PositionsListPage = () => {
     return (
         <React.Fragment>
+            <Grid container display="flex" spacing={2} direction="row" >
+                <Grid item id="profileSection" margin="20px 10px" >
+                    <ProfileSection/>
+                </Grid>
+                <Grid xs id="positionsPage">
             <PositionsListSection />
+            </Grid>
+        </Grid>
         </React.Fragment>
     );
 };
