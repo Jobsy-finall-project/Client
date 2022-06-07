835a37ed96008321af9ad655402a6d4ee788556e
Emipanda <eparkour@gmail.com>
2022-06-07 21:03:58 +0300

    buttons

--- src/pages/createRecruitmentTrackPage/CreateRecruitmentTrackPage.tsx
+++ src/pages/createRecruitmentTrackPage/CreateRecruitmentTrackPage.tsx
@@ -1,12 +1,8 @@
-import { Circle } from "@mui/icons-material";
 import React from "react";
-import Cycle from "../../components/common/cycle/Cycle";
 import CreateRecruitmentTrack from "../../components/form/forms/createRecruitmentTrack/CreateRecruitmentTrack";
 import TitleSection from "../../components/section/titleSection/TitleSection";
-import ThreeCircles from "../../components/threeCircles/ThreeCircles";
-import CreateRecruitmentTrackPageStyled from "./CreateRecruitmentTrackPageStyled";
-import * as Colors from "../../assets/Colors";
 import createTrack from "../../images/icon-tracking.png";
+import CreateRecruitmentTrackPageStyled from "./CreateRecruitmentTrackPageStyled";
 const CreateRecruitmentTrackPage = () => {
   return (
     <CreateRecruitmentTrackPageStyled>
